3352dd313c93d3dbcc61858302f83ea48c1ac978
Lucca D'Amico Diniz <lucca.damico@hotmail.com>
2023-09-09 10:45:32 -0400

    Inserção dos end-points de atualização do projeto e softdelete.

--- back-end/ledes/src/main/java/com/example/ledes/apresentacao/ProjetoController.java
+++ back-end/ledes/src/main/java/com/example/ledes/apresentacao/ProjetoController.java
@@ -4,12 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.ledes.aplicacao.AdicionarProjetoServico;
+import com.example.ledes.aplicacao.AtualizarProjetoServico;
+import com.example.ledes.aplicacao.DesativarProjetoServico;
 import com.example.ledes.infraestrutura.dto.ProjetoRequestDTO;
 import com.example.ledes.infraestrutura.dto.ProjetoResponseDTO;
 
@@ -22,6 +26,10 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 public class ProjetoController {
     @Autowired
     private AdicionarProjetoServico adicionarProjetoServico;
+    @Autowired
+    private AtualizarProjetoServico atualizarProjetoServico;
+    @Autowired
+    private DesativarProjetoServico desativarProjetoServico;
 
     @Operation(summary = "Criar um novo projeto")
     @ApiResponse(responseCode = "201")
@@ -30,4 +38,32 @@ public class ProjetoController {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(adicionarProjetoServico.adicionar(novoProjeto));
     }
+
+    @Operation(summary = "Atualizar um projeto")
+    @ApiResponse(responseCode = "200", description = "Retorna os dados atualizados")
+    @PutMapping(path = "/{id}", consumes = "application/json")
+    public ResponseEntity<ProjetoResponseDTO> atualizarProjeto(
+        @RequestBody ProjetoRequestDTO projetoRequestDTO, @PathVariable Long id){
+            ProjetoResponseDTO projetoAtualizado = atualizarProjetoServico.atualizar(id, projetoRequestDTO);
+
+            if (projetoAtualizado != null) {
+                return ResponseEntity.ok(projetoAtualizado);
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        }
+
+        @Operation(summary = "Desativar um projeto")
+        @ApiResponse(responseCode = "200", description = "Retorna os dados do projeto desativado")
+        @PostMapping(path = "/{id}/desativar", consumes = "application/json")
+        public ResponseEntity<ProjetoResponseDTO> desativarProjeto(@PathVariable Long id) {
+        ProjetoResponseDTO projetoDesativado = desativarProjetoServico.desativar(id);
+
+        if (projetoDesativado != null) {
+            return ResponseEntity.ok(projetoDesativado);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+}
+    
 }
