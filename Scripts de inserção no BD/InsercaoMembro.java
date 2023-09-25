5caf477e9c49b4b2f764faa8f7f05b33db8830e1
Diogo Felipe <67284614+diogof651@users.noreply.github.com>
2023-09-24 23:28:12 -0400

    Script de inserção de Membros no BD

---
+++ Scripts de inserção no BD/InsercaoMembro.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+
+public class MembroDataGenerator {
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        String[] nomes = {"João", "Maria", "Carlos", "Ana", "Pedro", "Mariana", "Lucas", "Laura", "Luiza", "Felipe"};
+        String[] extensoesFoto = {"jpg", "png", "jpeg"};
+
+        System.out.println("INSERT INTO Membro (nome, foto, ativo) VALUES");
+
+        for (int i = 1; i <= 20; i++) {
+            String nomeAleatorio = nomes[random.nextInt(nomes.length)];
+            String fotoAleatoria = "foto" + i + "." + extensoesFoto[random.nextInt(extensoesFoto.length)];
+            
+            boolean ativo = random.nextBoolean();
+            
+            System.out.printf("('%s', '%s', %b)", nomeAleatorio, fotoAleatoria, ativo);
+            
+            if (i < 20) {
+                System.out.print(",");
+            }
+            
+            System.out.println();
+        }
+    }
+}
