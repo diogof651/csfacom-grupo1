afecc98eba95c1fb3e937f9137b6e8e88973e1d5
Diogo Felipe <67284614+diogof651@users.noreply.github.com>
2023-09-30 22:56:19 -0400

    Adicionando o retorno do Anexo no EndPoint ObterNoticiaPorId.

--- back-end/ledes/src/main/java/com/example/ledes/aplicacao/noticia/ObterNoticiaPorIdServico.java
+++ back-end/ledes/src/main/java/com/example/ledes/aplicacao/noticia/ObterNoticiaPorIdServico.java
@@ -1,12 +1,17 @@
 package com.example.ledes.aplicacao.noticia;
 
+import java.util.Collection;
+import java.util.stream.Collectors; // Importe a classe Collectors
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.example.ledes.dominio.Noticia;
+import com.example.ledes.infraestrutura.AnexoRepositorio;
 import com.example.ledes.infraestrutura.NoticiaRepositorio;
+import com.example.ledes.infraestrutura.dto.AnexoDTO;
 import com.example.ledes.infraestrutura.dto.NoticiaResponseDTO;
 
 @Service
@@ -15,9 +20,18 @@ public class ObterNoticiaPorIdServico {
     @Autowired
     private NoticiaRepositorio noticiaRepositorio;
 
+    @Autowired
+    private AnexoRepositorio anexoRepositorio;
+
     public NoticiaResponseDTO obterNoticiaPorId(Long id) {
         Noticia noticia = noticiaRepositorio.findById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Notícia não encontrada"));
+        
+        // Use a classe Collectors para mapear os objetos Anexo para AnexoDTO
+        Collection<AnexoDTO> anexos = anexoRepositorio.findByNoticia(noticia)
+                .stream()
+                .map(anexo -> new AnexoDTO(anexo.getId(), anexo.getTitulo(), anexo.getConteudo()))
+                .collect(Collectors.toList());
 
         return new NoticiaResponseDTO(
                 noticia.getId(),
@@ -27,7 +41,7 @@ public class ObterNoticiaPorIdServico {
                 noticia.getEstado(),
                 noticia.getThumbnail(),
                 noticia.getDataPublicacao(),
-                noticia.getEmDestaque()
-                , null);
+                noticia.getEmDestaque(),
+                anexos);
     }
 }
